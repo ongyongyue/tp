48300dd2a658321faea9abe85f34e8bf7ad45ca1
Liu Zhiyuan <e1481827@u.nus.edu>
2026-03-17 17:24:21 +0800

    Add EditClientCommand to support editing client information

---
+++ src/main/java/seedu/address/logic/commands/EditClientCommand.java
@@ -0,0 +1,173 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.property.Property;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Edits the details of an existing client in the address book.
+ */
+public class EditClientCommand extends Command {
+    public static final String COMMAND_WORD = "editClient";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the client identified "
+            + "by the index number used in the displayed client list. "
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "[n/NAME] [c/PHONE] [e/EMAIL]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "p/91234567 e/johndoe@example.com";
+
+    public static final String MESSAGE_EDIT_CLIENT_SUCCESS = "Edited Client: %1$s";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This client already exists in the address book.";
+
+    private final Index index;
+    private final EditClientDescriptor editClientDescriptor;
+
+    /**
+     * @param index of the person in the filtered person list to edit
+     * @param editClientDescriptor details to edit the person with
+     */
+    public EditClientCommand(Index index, EditClientDescriptor editClientDescriptor) {
+        requireNonNull(index);
+        requireNonNull(editClientDescriptor);
+
+        this.index = index;
+        this.editClientDescriptor = new EditClientDescriptor(editClientDescriptor);
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = lastShownList.get(index.getZeroBased());
+        Person editedPerson = createEditedPerson(personToEdit, editClientDescriptor);
+
+        if (!personToEdit.isSamePerson(editedPerson) && model.hasPerson(editedPerson)) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
+
+        model.setPerson(personToEdit, editedPerson);
+        return new CommandResult(String.format(MESSAGE_EDIT_CLIENT_SUCCESS, editedPerson));
+    }
+
+    /**
+     * Creates and returns a {@code Person} with the details of {@code personToEdit}
+     * edited with {@code editClientDescriptor}.
+     */
+    private static Person createEditedPerson(Person personToEdit, EditClientDescriptor editClientDescriptor) {
+        assert personToEdit != null;
+
+        Name updatedName = editClientDescriptor.getName().orElse(personToEdit.getName());
+        Phone updatedPhone = editClientDescriptor.getPhone().orElse(personToEdit.getPhone());
+        Email updatedEmail = editClientDescriptor.getEmail().orElse(personToEdit.getEmail());
+        Set<Tag> updatedTags = personToEdit.getTags();
+        Set<Property> updatedProperties = personToEdit.getProperties();
+
+        return new Person(updatedName, updatedPhone, updatedEmail, updatedTags, updatedProperties);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof EditClientCommand)) {
+            return false;
+        }
+
+        EditClientCommand otherEditClientCommand = (EditClientCommand) other;
+        return index.equals(otherEditClientCommand.index)
+                && editClientDescriptor.equals(otherEditClientCommand.editClientDescriptor);
+    }
+
+    /**
+     * Stores the details to edit the person with. Each non-empty field value will replace the
+     * corresponding field value of the person.
+     */
+    public static class EditClientDescriptor {
+        private Name name;
+        private Phone phone;
+        private Email email;
+
+        public EditClientDescriptor() {}
+
+        /**
+         * Copy constructor.
+         */
+        public EditClientDescriptor(EditClientDescriptor toCopy) {
+            setName(toCopy.name);
+            setPhone(toCopy.phone);
+            setEmail(toCopy.email);
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(name, phone, email);
+        }
+
+        public void setName(Name name) {
+            this.name = name;
+        }
+
+        public Optional<Name> getName() {
+            return Optional.ofNullable(name);
+        }
+
+        public void setPhone(Phone phone) {
+            this.phone = phone;
+        }
+
+        public Optional<Phone> getPhone() {
+            return Optional.ofNullable(phone);
+        }
+
+        public void setEmail(Email email) {
+            this.email = email;
+        }
+
+        public Optional<Email> getEmail() {
+            return Optional.ofNullable(email);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other == this) {
+                return true;
+            }
+
+            if (!(other instanceof EditClientDescriptor)) {
+                return false;
+            }
+
+            EditClientDescriptor otherDescriptor = (EditClientDescriptor) other;
+            return Objects.equals(name, otherDescriptor.name)
+                    && Objects.equals(phone, otherDescriptor.phone)
+                    && Objects.equals(email, otherDescriptor.email);
+        }
+    }
+}
---
+++ src/test/java/seedu/address/logic/parser/EditClientCommandParserTest.java
@@ -0,0 +1,90 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_EMAIL_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_PHONE_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.PHONE_DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.PHONE_DESC_BOB;
+import static seedu.address.logic.commands.EditClientCommand.MESSAGE_NOT_EDITED;
+import static seedu.address.logic.commands.EditClientCommand.MESSAGE_USAGE;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.EditClientCommand;
+import seedu.address.logic.commands.EditClientCommand.EditClientDescriptor;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Phone;
+
+public class EditClientCommandParserTest {
+    private static final String MESSAGE_INVALID_FORMAT =
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE);
+
+    private final EditClientCommandParser parser = new EditClientCommandParser();
+
+    @Test
+    public void parse_missingParts_failure() {
+        assertParseFailure(parser, "1", MESSAGE_NOT_EDITED);
+        assertParseFailure(parser, NAME_DESC_AMY, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void parse_invalidPreamble_failure() {
+        assertParseFailure(parser, "a" + NAME_DESC_AMY, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "-1" + NAME_DESC_AMY, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "0" + NAME_DESC_AMY, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "1 some random string", MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void parse_invalidValue_failure() {
+        assertParseFailure(parser, "1" + INVALID_NAME_DESC, Name.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, "1" + INVALID_PHONE_DESC, Phone.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, "1" + INVALID_EMAIL_DESC, Email.MESSAGE_CONSTRAINTS);
+    }
+
+    @Test
+    public void parse_allFieldsSpecified_success() {
+        Index targetIndex = INDEX_SECOND_PERSON;
+        String userInput = targetIndex.getOneBased() + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB;
+
+        EditClientDescriptor descriptor = new EditClientDescriptor();
+        descriptor.setName(new Name("Bob Choo"));
+        descriptor.setPhone(new Phone("22222222"));
+        descriptor.setEmail(new Email("bob@example.com"));
+
+        EditClientCommand expectedCommand = new EditClientCommand(targetIndex, descriptor);
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_oneFieldSpecified_success() {
+        Index targetIndex = INDEX_FIRST_PERSON;
+
+        String userInput = targetIndex.getOneBased() + NAME_DESC_AMY;
+        EditClientDescriptor descriptor = new EditClientDescriptor();
+        descriptor.setName(new Name("Amy Bee"));
+        assertParseSuccess(parser, userInput, new EditClientCommand(targetIndex, descriptor));
+
+        userInput = targetIndex.getOneBased() + PHONE_DESC_AMY;
+        descriptor = new EditClientDescriptor();
+        descriptor.setPhone(new Phone("11111111"));
+        assertParseSuccess(parser, userInput, new EditClientCommand(targetIndex, descriptor));
+
+        userInput = targetIndex.getOneBased() + EMAIL_DESC_AMY;
+        descriptor = new EditClientDescriptor();
+        descriptor.setEmail(new Email("amy@example.com"));
+        assertParseSuccess(parser, userInput, new EditClientCommand(targetIndex, descriptor));
+    }
+}
